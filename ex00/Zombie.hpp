f4cf639df7737068b09c1a7675fb466274f66ef5
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 14:36:58 +1030

    Added changes

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,27 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:30:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 14:30:29 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:36:45 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+    private:
+        std::string _name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+
+        void announce() const;
+};
+
+Zombie* newZombie(std::string name);
+void    randomChump(std::string name);
+
+#endif
