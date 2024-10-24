6f31d8a56f663b5566660824436bdb7c419f941f
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 15:15:37 +1030

    Added changes

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,29 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:01:45 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:04:22 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:15:07 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
 int main(void)
 {
+    Zombie *horde;
+    int N;
+    int i;
+    N = 3;
+    i = 0;
+
+    horde = zombieHorde(N, "Horde");
+
+    while (i < N)
+    {
+        std::cout << "Index " << i << ": ";
+        horde[i].announce();
+        i++;
+    }
+
+    delete[] horde;
     return (0);
 }
