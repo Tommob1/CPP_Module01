b8bc45615ea5d21db9b020cc7304a34b7556fd42
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 15:18:14 +1030

    added changes

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:01:45 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:15:07 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:17:45 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
     Zombie *horde;
     int N;
     int i;
-    N = 3;
+    N = 5;
     i = 0;
 
     horde = zombieHorde(N, "Horde");
