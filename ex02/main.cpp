7568980ec617e022b6b88a2e3722ffe6caaf56ae
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 17:11:40 +1030

    Added changes to code

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:19:14 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 17:09:04 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 17:11:14 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,9 @@ int main(void)
     std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
     std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
     std::cout << std::endl;
+    std::cout << "The value of the string variable: " << string << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 
     return (0);
 }
