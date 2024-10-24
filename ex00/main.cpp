e4f8609f1d8517298ee1c7b6e3b28647e0f0ff34
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 14:46:42 +1030

    added changes

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,17 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:30:15 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 14:31:36 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:45:07 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+int main()
+{
+    Zombie* heapZombie = newZombie("HeapZombie");
+    heapZombie->announce();
+    delete heapZombie;
+    randomChump("StackZombie");
+    return(0);
+}
