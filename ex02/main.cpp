1d22dd52d4c13c46543a65a82c547fe5b14b9e7a
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 15:23:46 +1030

    Added changes to code

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,18 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:19:14 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:19:26 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:23:31 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+
+    std::cout << 
+}
