21093c5182e2e069cb20399f918d793eb64133ee
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 21:37:28 +1030

    Added changes to code

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,8 +6,21 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:21:32 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 17:22:34 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 21:37:11 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon)
+{
+}
+
+HumanA::~HumanA()
+{
+}
+
+void HumanA::attack()
+{
+    std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
+}
--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,7 +6,26 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:21:34 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 17:21:52 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 21:35:08 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include "Weapon.hpp"
+#include <string>
+#include <iostream>
+
+class HumanA
+{
+    private:
+        std::string _name;
+        Weapon &_weapon;
+    public:
+        HumanA(std::string name, Weapon &weapon);
+        ~HumanA();
+        void attack();
+};
+
+#endif
