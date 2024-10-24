cf304c5fd5065a32eb377a27d34615d0e5266a95
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 14:42:11 +1030

    Added changes

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,8 +6,31 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:30:25 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 14:31:40 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:41:16 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : _name(name) {}
+
+Zombie::~Zombie()
+{
+    std::cout << _name << " is destroyed" << std::endl;
+}
+
+void Zombie::announce() const
+{
+    std::cout << _name << ": Braiiiiiiinnnzzz..." << std::endl;
+}
+
+Zombie* newZombie(std::string name)
+{
+    return new Zombie(name);
+}
+
+void randomChump(std::string name)
+{
+    Zombie temp(name);
+    temp.announce();
+}
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:30:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 14:36:45 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:38:43 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define ZOMBIE_HPP
 
 #include <string>
+#include <iostream>
 
 class Zombie
 {
