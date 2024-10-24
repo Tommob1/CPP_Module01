2a2e0759bf79df152a9c5e1e7edf81e5a779bd5e
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 15:11:29 +1030

    Adde changes

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,8 +6,24 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:01:56 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:03:50 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:11:09 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name)
+{
+    int i;
+    i = 0;
+
+    Zombie *zombieHorde;
+    zombieHorde = new Zombie[N];
+
+    while(i < N)
+    {
+        zombieHorde[i] = Zombie(name);
+        i++;
+    }
+    return(zombieHorde);
+}
