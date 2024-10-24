7ede2d62f7aa90d86d8c8c055a3c0840ca477d36
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 14:54:49 +1030

    Added changes

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,17 +6,21 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:30:25 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 14:50:22 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:54:24 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name) : _name(name) {}
+Zombie::Zombie(std::string name) : _name(name)
+{
+    this->_name = name;
+    std::cout << _name << ": created" << std::endl;
+}
 
 Zombie::~Zombie()
 {
-    std::cout << _name << " is destroyed" << std::endl;
+    std::cout << _name << ": destroyed" << std::endl;
 }
 
 void Zombie::announce() const
