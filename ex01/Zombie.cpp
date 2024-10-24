96eda6c4128ab477950bae645cb678704c818a10
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 15:08:51 +1030

    Added changes

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,8 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:01:51 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:03:40 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:06:41 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie(){}
+
+Zombie::Zombie(std::string name)
+{
+    this->_name = name;
+}
+
+Zombie::~Zombie(){}
+
+void Zombie::announce()
+{
+    std::cout << _name << ": Braiiiiiiinnnzzzz..." << std::endl;
+}
--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:01:54 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 15:03:07 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:08:40 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,19 @@
 
 #include <string>
 #include <iostream>
+#include <new>
 
+class Zombie
+{
+    private:
+        std::string _name;
+    public:
+        Zombie();
+        Zombie(std::string name);
+        ~Zombie();
+        void announce(void);
+};
 
+Zombie *zombieHorde(int N, std::string name);
 
 #endif
