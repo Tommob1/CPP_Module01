40e800a4f4ff0cdb45b058ff69c45d29a4b5eb27
Brayden Tomlinson <humortronic@gmail.com>
2024-10-29 08:55:54 +1030

    Added changes

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 21:42:57 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/28 16:10:28 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:49:01 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,14 @@ int main(int argc, char **argv)
     std::string filename = argv[1];
     std::string search = argv[2];
     std::string replace = argv[3];
+
+    std::ifstream inputFile(filename);
+
+    if (!inputFile)
+    {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return (1);
+    }
     
     return (0);
 }
