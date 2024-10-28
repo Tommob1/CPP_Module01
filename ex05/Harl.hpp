d8b28f03b4fe2c2d3770430ff9a40cf05a2b432c
Brayden Tomlinson <humortronic@gmail.com>
2024-10-29 09:53:35 +1030

    Added changes

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,9 +6,12 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 09:16:19 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/29 09:38:32 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/29 09:53:20 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
 
 #include <iostream>
 #include <string>
@@ -17,8 +20,15 @@
 class Harl
 {
     private:
-    
+        void debug();
+        void info();
+        void warning();
+        void error();
+
     public:
         Harl();
         ~Harl();
+        void complain(const std::string& level);
 };
+
+#endif
