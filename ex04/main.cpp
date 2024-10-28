d79bdb822d2da3bbe2d6bfaa4c01e3e7c0a98e73
Brayden Tomlinson <humortronic@gmail.com>
2024-10-28 16:08:08 +1030

    Added changes to code

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 21:42:57 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/28 15:48:26 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/28 16:07:44 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@
 
 int main(int argc, char **argv)
 {
+    if (argc != 4)
+    {
+        std::serr << "Usage: ./replace <filename> <search> <replace>" << std::endl;
+        return (1);
+    }
     return (0);
 }
