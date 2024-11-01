3a59c1a8444cf4ae033469f7fffdca965ae18c85
Brayden Tomlinson <humortronic@gmail.com>
2024-11-01 17:57:12 +1100

    Added changes to code

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,8 +6,23 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:10:47 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/29 14:16:10 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 17:25:26 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./harlFilter <level>" << std::endl;
+        return (1);
+    }
+    Harl harl;
+    std::string level = argv[1];
+    harl.complain(level);
+    
+    return (0);
+}
