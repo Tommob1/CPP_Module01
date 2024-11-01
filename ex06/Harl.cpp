ce96a42ad45984c61e0eec530fdcd004d604db8d
Brayden Tomlinson <humortronic@gmail.com>
2024-11-01 17:49:22 +1100

    Added changes to code

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,8 +6,47 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:10:38 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/29 14:16:08 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 17:18:58 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl() {}
+Harl::~Harl() {}
+
+void Harl::debug()
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info() {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!" << std::endl;
+}
+
+void Harl::warning() {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error() {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(const std::string& level)
+{
+    int levelIndex;
+    if (level == "DEBUG") levelIndex = 0;
+    else if (level == "INFO") levelIndex = 1;
+    else if (level == "WARNING") levelIndex = 2;
+    else if (level == "ERROR") levelIndex = 3;
+    else
+    {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+        return;
+    }
+}
--- ex06/Harl.hpp
+++ ex06/Harl.hpp
@@ -6,7 +6,27 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:10:44 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/29 14:10:44 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 17:14:50 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+    private:
+        void debug();
+        void info();
+        void warning();
+        void error();
+
+    public:
+        Harl();
+        ~Harl();
+        void complain(const std::string& level);
+};
+
+#endif
